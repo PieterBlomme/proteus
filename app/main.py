7ed007e7284e948532c1e03e92ed3fe05b68a24e
pieter blomme <pieter.blomme@gmail.com>
2020-09-26 17:59:07 +0200

    basic api operations

--- app/main.py
+++ app/main.py
@@ -21,18 +21,37 @@ except Exception as e:
     logger.error("client creation failed: " + str(e))
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/health")
+async def get_server_health():
+    if triton_client.is_server_live():
+        logger.info(f'Server is alive')
+        return {"success": True}
+    else:
+        logger.info(f'Server is dead')
+        return {"success": False}
 
+@app.get("/models")
+async def get_model_repository():
+    return triton_client.get_model_repository_index()
 
-@app.get("/load_model/")
+@app.get("/load/")
 async def load_model(model: str):
-
     if model == 'yolov4':
-        logger.error('before load')
+        logger.info(f'Loading model {model}')
         triton_client.load_model(model)
-        logger.error('after load')
-        return {"success": True, "message": f"model {model} loaded"}
+        if not triton_client.is_model_ready(model):
+            return {"success": False, "message": f"model {model} not ready - check logs"}
+        else:
+            return {"success": True, "message": f"model {model} loaded"}
     else:
         return {"success": False, "message": "unknown model"}
+
+@app.get("/unload/")
+async def unload_model(model: str):
+    if not triton_client.is_model_ready(model):
+        logger.info(f'No model with name {model} loaded')
+        return {"success": False, "message": "model not loaded"}
+    else:
+        logger.info(f'Unloading model {model}')
+        triton_client.unload_model(model)
+        return {"success": True, "message": f"model {model} unloaded"}
