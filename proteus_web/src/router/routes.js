aa9d228798cb05baac911957d7b0a5ce10978116
pieter blomme <pieter.blomme@gmail.com>
2020-09-28 20:51:52 +0200

    remove some stuff that we don't need

--- proteus_web/src/router/routes.js
+++ proteus_web/src/router/routes.js
@@ -4,11 +4,7 @@ import NotFound from "@/pages/NotFoundPage.vue";
 
 // Admin pages
 const Dashboard = () => import(/* webpackChunkName: "dashboard" */"@/pages/Dashboard.vue");
-const Profile = () => import(/* webpackChunkName: "common" */ "@/pages/Profile.vue");
 const Notifications = () => import(/* webpackChunkName: "common" */"@/pages/Notifications.vue");
-const Icons = () => import(/* webpackChunkName: "common" */ "@/pages/Icons.vue");
-const Maps = () => import(/* webpackChunkName: "common" */ "@/pages/Maps.vue");
-const Typography = () => import(/* webpackChunkName: "common" */ "@/pages/Typography.vue");
 const TableList = () => import(/* webpackChunkName: "common" */ "@/pages/TableList.vue");
 
 const routes = [
@@ -23,29 +19,9 @@ const routes = [
         component: Dashboard
       },
       {
-        path: "profile",
-        name: "profile",
-        component: Profile
-      },
-      {
         path: "notifications",
         name: "notifications",
         component: Notifications
-      },
-      {
-        path: "icons",
-        name: "icons",
-        component: Icons
-      },
-      {
-        path: "maps",
-        name: "maps",
-        component: Maps
-      },
-      {
-        path: "typography",
-        name: "typography",
-        component: Typography
       },
       {
         path: "table-list",
