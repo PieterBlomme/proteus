d5bdaa30c0ee55f2c5a68646886fc3f777dd091c
pieter blomme <pieter.blomme@gmail.com>
2020-10-12 17:25:19 +0200

    fix the other classifiers

--- packages/proteus_resnet50/proteus/resnet50/client.py
+++ packages/proteus_resnet50/proteus/resnet50/client.py
@@ -12,6 +12,8 @@ class Resnet50(ClassificationModel):
     MODEL_NAME = "resnet50"
     CHANNEL_FIRST = True
     CLASSES = read_class_names(f"{folder_path}/imagenet_labels.txt")
+    MODEL_URL = "https://github.com/onnx/models/raw/master/vision/classification/resnet/model/resnet50-v2-7.onnx"
 
 
 inference_http = Resnet50.inference_http
+load_model = Resnet50.load_model
