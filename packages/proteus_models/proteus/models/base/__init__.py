f13b591a7c6971dfd9ac583387dc1ae1b5a89e90
pieter blomme <pieter.blomme@gmail.com>
2020-10-26 21:45:26 +0100

    tiny fix

--- packages/proteus_models/proteus/models/base/__init__.py
+++ packages/proteus_models/proteus/models/base/__init__.py
@@ -2,9 +2,10 @@ __author__ = """Pieter Blomme"""
 __email__ = "pieter.blomme@robovision.eu"
 __version__ = "0.0.1"
 
+from .base import BaseModel
 from .classification import ClassificationModel
 from .detection import DetectionModel
 
-__all__ = ["DetectionModel", "ClassificationModel"]
+__all__ = ["BaseModel", "DetectionModel", "ClassificationModel"]
 
 model_dict = {}
