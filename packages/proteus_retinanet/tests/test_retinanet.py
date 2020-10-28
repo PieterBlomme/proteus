295d1ed78c51a26f5ab574e823296f0f3a9adda9
pieter blomme <pieter.blomme@gmail.com>
2020-10-28 21:41:57 +0100

    lint

--- packages/proteus_retinanet/tests/test_retinanet.py
+++ packages/proteus_retinanet/tests/test_retinanet.py
@@ -22,9 +22,11 @@ def model():
 def dataset():
     return CocoVal(k=50)
 
+
 @pytest.fixture
 def small_dataset():
     return CocoVal(k=10)
+
 
 def test_speed(dataset, model):
     fpath, _ = dataset[0]
@@ -116,8 +118,9 @@ def test_score(dataset, model):
     mAP = dataset.eval(preds)
     assert mAP > 0.25
 
+
 def test_resize(small_dataset, model):
-    #mAP should be similar after doubling image size
+    # mAP should be similar after doubling image size
     preds_normal = []
     preds_resize = []
     for (fpath, img) in small_dataset:
@@ -146,10 +149,10 @@ def test_resize(small_dataset, model):
                         preds_normal.append(result)
                 except Exception as e:
                     print(e)
-        
+
         tmp_img = Image.open(fpath)
         w, h = tmp_img.size
-        tmp_img.resize((w*2,h*2)).save(fpath)
+        tmp_img.resize((w * 2, h * 2)).save(fpath)
         with open(fpath, "rb") as f:
             jsonfiles = {"file": f}
             payload = {"file_id": fpath}
@@ -159,17 +162,17 @@ def test_resize(small_dataset, model):
                 data=payload,
             )
             for box in response.json()[0]:
-                #half every box
+                # half every box
                 try:
                     result = {
                         "image_id": img["id"],
                         "category_id": small_dataset.cats[box["class_name"]],
                         "score": box["score"],
                         "bbox": [
-                            box["x1"] /2,
-                            box["y1"] /2,
-                            box["x2"] /2 - box["x1"] /2,
-                            box["y2"] /2 - box["y1"] /2,
+                            box["x1"] / 2,
+                            box["y1"] / 2,
+                            box["x2"] / 2 - box["x1"] / 2,
+                            box["y2"] / 2 - box["y1"] / 2,
                         ],
                     }
                     if box["score"] > 0.2:
@@ -179,11 +182,11 @@ def test_resize(small_dataset, model):
     mAP_normal = small_dataset.eval(preds_normal)
     mAP_resize = small_dataset.eval(preds_resize)
     print(abs(mAP_normal - mAP_resize))
-    assert abs(mAP_normal - mAP_resize) < 0.02 # 2% diff seems acceptable
+    assert abs(mAP_normal - mAP_resize) < 0.02  # 2% diff seems acceptable
 
 
 def test_padding(small_dataset, model):
-    #mAP should be similar after padding to a square
+    # mAP should be similar after padding to a square
     preds_normal = []
     preds_padded = []
     for (fpath, img) in small_dataset:
@@ -212,12 +215,12 @@ def test_padding(small_dataset, model):
                         preds_normal.append(result)
                 except Exception as e:
                     print(e)
-        
+
         tmp_img = Image.open(fpath)
         w, h = tmp_img.size
-        target = max((w,h))
-        dw = (target-w) /2
-        dh = (target-h) /2
+        target = max((w, h))
+        dw = (target - w) / 2
+        dh = (target - h) / 2
         tmp_img = pad(tmp_img, (target, target))
         tmp_img.save(fpath)
         with open(fpath, "rb") as f:
@@ -235,8 +238,8 @@ def test_padding(small_dataset, model):
                         "category_id": small_dataset.cats[box["class_name"]],
                         "score": box["score"],
                         "bbox": [
-                            box["x1"]-dw,
-                            box["y1"]-dh,
+                            box["x1"] - dw,
+                            box["y1"] - dh,
                             box["x2"] - box["x1"],
                             box["y2"] - box["y1"],
                         ],
@@ -248,4 +251,4 @@ def test_padding(small_dataset, model):
     mAP_normal = small_dataset.eval(preds_normal)
     mAP_padded = small_dataset.eval(preds_padded)
     print(abs(mAP_normal - mAP_padded))
-    assert abs(mAP_normal - mAP_padded) < 0.05 # 5% diff seems acceptable
+    assert abs(mAP_normal - mAP_padded) < 0.05  # 5% diff seems acceptable
