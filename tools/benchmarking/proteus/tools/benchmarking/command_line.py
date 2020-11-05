4d5978cbc43ac01f9822b7868c8b97b99693a471
pieter blomme <pieter.blomme@gmail.com>
2020-11-05 21:26:22 +0100

    lint

--- tools/benchmarking/proteus/tools/benchmarking/command_line.py
+++ tools/benchmarking/proteus/tools/benchmarking/command_line.py
@@ -2,8 +2,9 @@ import importlib
 import json
 import os
 import time
-import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import requests
 
 mod = importlib.import_module("proteus.datasets")
 
@@ -16,6 +17,7 @@ def load_model(model):
 def load_dataset(dataset, num_samples):
     dataset = getattr(mod, dataset)
     return dataset(k=num_samples)
+
 
 def get_prediction(fpath, model):
     with open(fpath, "rb") as f:
@@ -47,21 +49,20 @@ def main():
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
         # Start the load operations and mark each future with its URL
         futures = [
-                executor.submit(get_prediction, fpath, model)
-                for (fpath, img) in dataset
-                ]
+            executor.submit(get_prediction, fpath, model) for (fpath, img) in dataset
+        ]
         for fut in as_completed(futures):
             response = fut.result()
             preds.append(response.json()[0])
     score = dataset.eval(preds)
     end = time.time()
-    throughput = (end - start)*1000 / num_samples
+    throughput = (end - start) * 1000 / num_samples
 
-    #latency
+    # latency
     fpath, img = dataset[0]
-    latency = get_prediction(fpath, model).elapsed.total_seconds()*1000
+    latency = get_prediction(fpath, model).elapsed.total_seconds() * 1000
 
-    print('Results')
-    print(f'Throughput: {throughput} FPS')
-    print(f'Latency: {latency} ms')
-    print(f'Score: {score}')
+    print("Results")
+    print(f"Throughput: {throughput} FPS")
+    print(f"Latency: {latency} ms")
+    print(f"Score: {score}")
