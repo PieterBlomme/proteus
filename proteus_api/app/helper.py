dfd5ffdf412133763b0e2d2ac3c101b120d6c592
pieter blomme <pieter.blomme@gmail.com>
2020-12-15 21:23:51 +0100

    Fix loglevels

--- proteus_api/app/helper.py
+++ proteus_api/app/helper.py
@@ -50,7 +50,7 @@ def get_model_dict():
     for finder, name, ispkg in iter_namespace(proteus.models):
         module = importlib.import_module(name)
         model_dict.update(module.model_dict)
-    logger.info(model_dict)
+    logger.debug(model_dict)
     return model_dict
 
 
