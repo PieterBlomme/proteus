fa70afda030b8bc475551687cbff18857bc3aa94
pieter blomme <pieter.blomme@gmail.com>
2020-12-30 13:52:27 +0100

    Update template

--- tools/templating/proteus/tools/templating/templates/proteus.models.{{cookiecutter.package_name}}/setup.py
+++ tools/templating/proteus/tools/templating/templates/proteus.models.{{cookiecutter.package_name}}/setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=requirements,
     long_description=readme,
     include_package_data=True,
-    namespace_packages=["proteus.models"],
+    namespace_packages=["proteus", "proteus.models"],
     packages=find_namespace_packages(exclude=["tests"]),
     test_suite="tests",
     tests_require=test_requirements,
