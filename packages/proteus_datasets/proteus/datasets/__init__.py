7c59847f04c8658d98c8a97d92baa614663b01ce
pieter blomme <pieter.blomme@gmail.com>
2021-01-07 21:29:30 +0100

    Lint

--- packages/proteus_datasets/proteus/datasets/__init__.py
+++ packages/proteus_datasets/proteus/datasets/__init__.py
@@ -2,9 +2,9 @@ __author__ = """Pieter Blomme"""
 __email__ = "pieter.blomme@gmail.com"
 __version__ = "0.0.1"
 
-from .dataset import Dataset
 from .bsds import BSDSSuperRes
 from .coco import CocoValBBox, CocoValMask
+from .dataset import Dataset
 from .imagenette import ImageNette
 from .mpii import MPIIPoseEstimation
 
